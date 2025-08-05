9885bb6518490bd7c814c9a4c2d92170ab5e10ff
Franciele Franco <fran.alves1108@gmail.com>
2025-08-05 17:26:06 -0300

    project 1introduction to web programming

---
+++ fundamentos-modern-web/Introdução-programação-web/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', () => {
+  const msg = document.getElementById('message');
+  msg.textContent = 'Desenvolvido por Franciele Franco como parte do curso iTalents';
+  msg.style.color = '#39796b';
+});
